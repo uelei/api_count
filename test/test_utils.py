0f8e249fbe5c6b74f76badf549dec9bbf697239b
uelei <github@uelei.com>
2017-01-15 23:15:10 +0000

    add tests for api

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 __author__ = 'wesleywwerneck'
-
+import pytest
+from api_count import exceptions
 from api_count import utils
 
-def test_word_count():
 
+def test_word_count():
+    """
+        Unit test for method word count
+    """
     word = "gol"
 
     text = """ kombi fusca gol corsa fiat uno Gol corrola"""
@@ -19,7 +23,12 @@ def test_word_count():
 
 
 def test_checkurl():
+    """
+        Unit test to validate url
+    """
+    assert None == utils.check_url("http://blog.uelei.com")
 
-    assert utils.check_url("http://blog.uelei.com")
+    with pytest.raises(exceptions.InvalidUrl) as error_info:
+        utils.check_url("salslallasldalslda:///dasda")
 
-    assert not utils.check_url("salslallasldalslda:///dasda")
+    assert error_info.value.to_dict() == {'error': 'Invalid url'}
